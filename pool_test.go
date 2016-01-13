2767b5bbadfa3f359dafd4daaa7c90fe4466f521
lrsec <lrsecly@gmail.com>
2016-01-13 13:55:26 +0800

    update

--- pool_test.go
+++ pool_test.go
@@ -98,3 +98,39 @@ func TestNewPool(t *testing.T) {
 		t.Errorf("data not treated correctly with outbound len: %d\n", length)
 	}
 }
+
+func TestWithPanic(t *testing.T) {
+	inboundChannel := make(chan interface{}, 10)
+	defer close(inboundChannel)
+
+	pool, err := NewPool(10, 20, 15, 500, 10, inboundChannel, func(input interface{}) interface{} {
+		panic("test")
+	})
+
+	if err != nil {
+		t.Error("Can not create pool", err)
+	}
+
+	defer func() {
+		if pool != nil {
+			pool.Close()
+		}
+	}()
+
+	for i := 0; i < 10; i++ {
+		pool.InboundChannel <- i
+	}
+
+	time.Sleep(2 * time.Second)
+
+	inLength := len(pool.InboundChannel)
+	outLength := len(pool.OutboundChannel)
+
+	if inLength != 0 {
+		t.Errorf("data not treated correctly with inbound len: %d\n", inLength)
+	}
+
+	if outLength != 0 {
+		t.Errorf("data not treated correctly with outbound len: %d\n", outLength)
+	}
+}
